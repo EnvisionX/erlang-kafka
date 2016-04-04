3afb0a84e808d3880f90b11ca6f7319cd237c8d5
Aleksey Morarash <aleksey.morarash@gmail.com>
2016-04-04 22:01:21 +0300

    add metadata/1 convenient wrapper in the main module

--- src/kafka.erl
+++ src/kafka.erl
@@ -15,6 +15,7 @@
 %% API exports
 -export(
    [metadata/2,
+    metadata/1,
     crd/7
    ]).
 
@@ -41,7 +42,7 @@
 
 %% @doc Connect to broker, make a metadata request for all existing
 %% topics and disconnect.
-%% This function is convenient wrapper to make a bootstrapping
+%% This function is a convenient wrapper to make a bootstrapping
 %% metadata request to a Kafka broker.
 -spec metadata(Host :: kafka_socket:host(),
                Port :: inet:port_number()) ->
@@ -55,6 +56,26 @@ metadata(Host, Port) ->
             {ok, Brokers, Topics};
         {error, _Reason} = Error ->
             Error
+    end.
+
+%% @doc Fetch metadata from first available Kafka broker.
+%% This function is a convenient wrapper to make a bootstrapping
+%% metadata request to a Kafka broker.
+-spec metadata([{kafka_socket:host(), inet:port_number()}]) ->
+                      {Brokers :: list(), Topics :: list()} |
+                      undefined.
+metadata([]) ->
+    undefined;
+metadata([{Host, Port} | Tail]) ->
+    case metadata(Host, Port) of
+        {ok, [] = _Brokers, _Topics} ->
+            %% This strange behaviour I watched with kafka 0.8.2.2.
+            %% Try next node.
+            metadata(Tail);
+        {ok, Brokers, Topics} ->
+            {Brokers, Topics};
+        {error, _Reason} ->
+            metadata(Tail)
     end.
 
 %% @doc Connect to broker, make a request and disconnect.
